bf49337adb282257f55560b05cf201d7946adb15
Luiz Brito <luizbsilva4@gmail.com>
2024-11-14 19:31:35 -0300

    create project

---
+++ src/main/java/com/luiz/g_store_api/GStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.luiz.g_store_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GStoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GStoreApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luiz/g_store_api/GStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiz.g_store_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
